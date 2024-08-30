f3dc8807eb01f7164cfc1479a3c91a813eea611b
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-08-30 16:43:18 +0530

    Further breaking into shared components for button and using host styling and creating new-ticket component to add support tickets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
-import { DashboardComponentsComponent } from "./dashboard/dashboard.component";
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { SupportTicketsComponent } from './dashboard/support-tickets/support-tickets.component';
+import { TrafficComponentComponent } from './dashboard/traffic-component/traffic-component.component';
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, DashboardComponentsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, SupportTicketsComponent, TrafficComponentComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
