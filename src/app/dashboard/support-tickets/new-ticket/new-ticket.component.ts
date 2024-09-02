8e8aa07fcce6b2b20925e84895c8577805161aa6
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-09-02 12:12:41 +0530

    Used more lifecycle hooks like afterViewChild, used viewChild signal and added the ticket component to display ticket information after submitting the form

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, output, viewChild, ViewChild } from '@angular/core';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  add = output<{title: string; text: string}>();
 
+  ngOnInit() {
+    console.log('OnInit');
+    console.log(this.form().nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('After View Init')
+    console.log(this.form().nativeElement);
+  }
+
+  onSubmit(title: string, text: string) {
+    this.add.emit({ title, text });
+    this.form().nativeElement.reset();
+  }
 }
